0369b8fdcd1fd47977bca4ad2e1786090f35d198
Shahar Sukenik <shaharsu@gmail.com>
2024-10-07 16:40:55 -0400

    Updated Fig. 4 and associated SI

--- Fig.3/Fig.3LiP.py
+++ Fig.3/Fig.3LiP.py
@@ -19,7 +19,7 @@ mpl.rcParams['font.size'] = 20
 mpl.rcParams['svg.fonttype'] = 'none'
 
 #%%
-xl = pd.ExcelFile('Fig.3.LiP.2.xlsx')
+xl = pd.ExcelFile('Fig.3.LiP.xlsx')
 cmap = ['green','purple','orange','red','blue']
 categories = ['log(abundance)','length','Domains','% disordered','log(interactors)']
 fig,ax=plt.subplots(1,len(categories),figsize=[4*len(categories),5],sharey=True)
@@ -35,9 +35,9 @@ ax[0].set_ylabel('fraction of \n perturbed peptides')
 plt.tight_layout()
 fig.savefig('Fig.3G-K.svg')
 #%%
-categories = ['Cellular location','CCT binding sites','SSB binding sites','pI']
+categories = ['pI','CCT binding sites','SSB binding sites','Cellular location']
 cmap = plt.cm.rainbow(np.linspace(0,1,len(categories)))
-fig,ax=plt.subplots(1,len(categories),figsize=[4*len(categories),5])
+fig,ax=plt.subplots(1,len(categories),figsize=[4*len(categories),7])
 for i,sheet_name in enumerate(categories):
     data = xl.parse(sheet_name,index_col=0)
     # ticks = np.a
@@ -52,64 +52,4 @@ for i,sheet_name in enumerate(categories):
         ax[i].set_xticklabels(data.index.astype('string').str.split('-').str[0][::2])
 ax[0].set_ylabel('fraction of \n perturbed peptides')
 plt.tight_layout()
-fig.savefig('Fig.SX.svg')
-
-#%%
-N_bins=8
-cmap = plt.cm.Purples(np.linspace(0.2,1,N_bins))
-data = pd.read_csv('Fig.3.LiP.csv',index_col=0)
-categories = ['Median Abundance','Isoelectric Point','Percent Disordered','Length','CCT sites','Log(Interactors)','pSup_42C',]
-fig,ax = plt.subplots(1,len(categories),figsize=[5*len(categories),5],sharey=True)
-for i,cat in enumerate(categories):
-    sliced=data.dropna(subset=cat)
-    sliced.loc[:,cat] = pd.to_numeric(sliced[cat], errors='coerce').dropna()
-    bins = sliced[cat].quantile(np.linspace(1/N_bins,1,N_bins)).values
-    total = sliced.loc[sliced[cat]<bins[0],'No. of Valid Peptides'].sum()
-    modified = sliced.loc[sliced[cat]<bins[0],'No. of Significant Peptides (Adj. P-value)'].sum()
-    ax[i].bar(0,modified/total,color=cmap[i],edgecolor='k',lw=1)
-    for i_bin in np.arange(1,len(bins)):
-        ax[i].plot([i_bin-0.5,i_bin-0.5],[0,1],'--',color='grey')
-        total = sliced.loc[(sliced[cat]>bins[i_bin-1])&(sliced[cat]<bins[i_bin]),'No. of Valid Peptides'].sum()
-        modified = sliced.loc[(sliced[cat]>bins[i_bin-1])&(sliced[cat]<bins[i_bin]),'No. of Significant Peptides (Adj. P-value)'].sum()
-        ax[i].text((i_bin)/(N_bins),1,str("{0:.2g}".format(bins[i_bin-1])),rotation=90,ha='right',va='top',fontsize=16,transform = ax[i].transAxes)
-        ax[i].bar(i_bin,modified/total,color=cmap[i],edgecolor='k',lw=1)
-    ax[i].set_xticks(np.arange(i_bin+1))
-    ax[i].set_xticklabels(np.arange(i_bin+1)+1)
-    ax[i].set_xlabel(cat)
-    ax[i].set_xlim(-0.5,N_bins-0.5)
-    ax[i].set_ylim(0,0.40)
-ax[0].set_ylabel('fraction of sig. peptides')
-#%%
-
-cmap = ['lightgreen','purple','orange','red','blue']
-data = pd.read_csv('Fig.3.LiP.csv',index_col=0)
-categories = ['Median Abundance','Length','Number of Domains','Percent Disordered','Interactors']
-minmax = [[0,9000,8],[1,2000,15],[-1,5,7],[0,100,15],[0,150,9]]
-fig,ax = plt.subplots(1,len(categories),figsize=[3.5*len(categories),4],sharey=False)
-fig.subplots_adjust(hspace=0.2, wspace=0.7)
-for i,cat in enumerate(categories):
-    print(cat)
-    sliced=data.dropna(subset=cat)
-    sliced.loc[:,cat] = pd.to_numeric(sliced[cat], errors='coerce').dropna()
-    bins = np.linspace(minmax[i][0],minmax[i][1],minmax[i][2]).astype('float')
-    width = 0.8*(bins[1]-bins[0])
-    total = sliced.loc[sliced[cat]<=bins[0],'No. of Valid Peptides'].sum()
-    modified = sliced.loc[sliced[cat]<=bins[0],'No. of Significant Peptides (Adj. P-value)'].sum()
-    print(bins[0])
-    print(total)
-    ax[i].bar(bins[0],modified/total,color=cmap[i],edgecolor='k',lw=1,width=width,alpha=0.4)
-    for i_bin in np.arange(1,len(bins)):
-        total = sliced.loc[(sliced[cat]>bins[i_bin-1])&(sliced[cat]<=bins[i_bin]),'No. of Valid Peptides'].sum()
-        modified = sliced.loc[(sliced[cat]>bins[i_bin-1])&(sliced[cat]<=bins[i_bin]),'No. of Significant Peptides (Adj. P-value)'].sum()
-        print(bins[i_bin])
-        print(total)
-        # ax[i].text(1/(N_bins-1)*(i_bin-0.5),1,str(int(total)),rotation=90,ha='center',va='top',fontsize=16,transform = ax[i].transAxes)
-        ax[i].bar(bins[i_bin],modified/total,color=cmap[i],edgecolor='k',lw=1,width=width,alpha=0.4)
-        # ax[i].plot(np.ones(2)*bins[i_bin]+width*.65,[0,1],'--',color='grey')
-    # ax[i].set_xticks(np.arange(i_bin+1))
-    # ax[i].set_xticklabels(np.arange(i_bin+1)+1)
-    ax[i].set_xlabel(cat)
-    ax[i].set_xlim(bins.min()+width*.65,bins.max()+width*.65)
-    # ax[i].set_ylim(0,0.5)
-ax[0].set_ylabel('fraction of \n perturbed peptides')
-# plt.tight_layout()
+fig.savefig('Fig.S4.svg')
---
+++ Fig.4/Fig.4LiP.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 09:23:19 2024
+
+@author: ssukenik
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['axes.linewidth'] = 3
+mpl.rcParams['ytick.major.size'] = 10
+mpl.rcParams['xtick.major.size'] = 10
+mpl.rcParams['ytick.major.width'] = 3
+mpl.rcParams['xtick.major.width'] = 3
+mpl.rcParams['font.size'] = 20
+mpl.rcParams['svg.fonttype'] = 'none'
+
+#%%
+xl = pd.ExcelFile('Fig.4.LiP.DODO.xlsx')
+#%%
+cmap = ['green','purple','orange','red','blue','pink']
+categories = xl.sheet_names
+#categories = ['log(abundance)','length','Domains','% disordered','log(interactors)']
+fig,ax=plt.subplots(1,len(categories),figsize=[4*len(categories),5],sharey=True)
+for i,sheet_name in enumerate(categories):
+    data = xl.parse(sheet_name,index_col=0)
+    # ticks = np.a
+    ax[i].bar(np.arange(len(data)),data['structurally perturbed peptide fraction'],
+              color=cmap[i],alpha=0.4)
+    ax[i].set_xlabel(sheet_name)
+    ax[i].set_xticks([0,int(len(data)/2),len(data)-1])
+    ax[i].text(0.1,0.9,'DODO',transform=ax[i].transAxes)
+    ax[i].set_xticklabels(data.index.astype('string').str.split('-').str[0][[0,int(len(data)/2),len(data)-1]])
+ax[0].set_ylabel('fraction of \n perturbed peptides')
+plt.tight_layout()
+fig.savefig('Fig.S6F.svg')
+#%%
+fig,ax = plt.subplots(1,2,figsize=[12,2.5],sharey=True)
+fig.subplots_adjust(wspace=0.15)
+data = xl.parse('percentAGP',index_col=0)
+ax[1].plot(np.arange(len(data)),data['structurally perturbed peptide fraction'],'--',c='purple')
+ax[1].scatter(np.arange(len(data)),data['structurally perturbed peptide fraction'],s=300,edgecolor='k',c='purple')
+ax[1].set_xticks(np.arange(0,len(data))[::2])
+ax[1].set_xticklabels(data.index[::2],rotation=45)
+ax[1].set_ylabel('')
+
+data = xl.parse('percentDE',index_col=0)
+ax[0].plot(np.arange(len(data)),data['structurally perturbed peptide fraction'],'--',c='r')
+ax[0].scatter(np.arange(len(data)),data['structurally perturbed peptide fraction'],s=300,edgecolor='k',c='r')
+data = xl.parse('percentKR',index_col=0)
+ax[0].plot(np.arange(len(data)),data['structurally perturbed peptide fraction'],'--',c='b')
+ax[0].scatter(np.arange(len(data)),data['structurally perturbed peptide fraction'],s=300,edgecolor='k',c='b')
+ax[0].set_xticks(np.arange(1,len(data)))
+ax[0].set_xticklabels(data.index[1:],rotation=45)
+ax[0].set_ylabel('fraction of\nperturbed peptides')
+fig.savefig('Fig.4F.svg')
+
+#%%
+xl = pd.ExcelFile('Fig.4.LiP.chainsaw.xlsx')
+#%%
+cmap = ['green','purple','orange','red','blue','pink']
+categories = xl.sheet_names
+#categories = ['log(abundance)','length','Domains','% disordered','log(interactors)']
+fig,ax=plt.subplots(1,len(categories),figsize=[4*len(categories),5],sharey=True)
+for i,sheet_name in enumerate(categories):
+    data = xl.parse(sheet_name,index_col=0)
+    # ticks = np.a
+    ax[i].bar(np.arange(len(data)),data['structurally perturbed peptide fraction'],
+              color=cmap[i],alpha=0.4)
+    ax[i].set_xlabel(sheet_name)
+    ax[i].set_xticks([0,int(len(data)/2),len(data)-1])
+    ax[i].set_xticklabels(data.index.astype('string').str.split('-').str[0][[0,int(len(data)/2),len(data)-1]])
+    ax[i].text(0.1,0.9,'chainsaw',transform=ax[i].transAxes)
+ax[0].set_ylabel('fraction of \n perturbed peptides')
+ax[i].text(0.1,0.9,'chainsaw',transform=ax[0].transAxes)
+plt.tight_layout()
+fig.savefig('Fig.S6E.svg')
+#%%
+fig,ax = plt.subplots(1,2,figsize=[12,2.5],sharey=True)
+fig.subplots_adjust(wspace=0.15)
+data = xl.parse('percentAGP',index_col=0)
+ax[1].plot(np.arange(len(data)),data['structurally perturbed peptide fraction'],'--',c='purple')
+ax[1].scatter(np.arange(len(data)),data['structurally perturbed peptide fraction'],s=300,edgecolor='k',c='purple')
+ax[1].set_xticks(np.arange(0,len(data))[::2])
+ax[1].set_xticklabels(data.index[::2],rotation=45)
+ax[1].text(0.05,0.05,'chainsaw',transform=ax[1].transAxes)
+ax[1].set_ylabel('')
+
+data = xl.parse('percentDE',index_col=0)
+ax[0].plot(np.arange(len(data)),data['structurally perturbed peptide fraction'],'--',c='r')
+ax[0].scatter(np.arange(len(data)),data['structurally perturbed peptide fraction'],s=300,edgecolor='k',c='r')
+data = xl.parse('percentKR',index_col=0)
+ax[0].plot(np.arange(len(data)),data['structurally perturbed peptide fraction'],'--',c='b')
+ax[0].scatter(np.arange(len(data)),data['structurally perturbed peptide fraction'],s=300,edgecolor='k',c='b')
+ax[0].set_xticks(np.arange(1,len(data)))
+ax[0].set_xticklabels(data.index[1:],rotation=45)
+ax[0].text(0.05,0.85,'chainsaw',transform=ax[0].transAxes)
+ax[0].set_ylabel('fraction of\nperturbed peptides')
+fig.savefig('Fig.S6G.svg')
